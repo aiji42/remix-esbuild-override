96be0beb751a1b52fe5f489923dc2ae91947fec9
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-15 21:57:47 +0900

    feat: add replaceEsbuild - script to replace esbuild

---
+++ src/replace.ts
@@ -0,0 +1,32 @@
+import { execSync } from "node:child_process";
+
+export const replaceEsbuild = () => {
+  const isOverridden = require
+    .resolve("esbuildあ")
+    .endsWith("remix-esbuild-override/dist/index.js");
+  if (isOverridden) {
+    console.log(
+      "💽 esbuild has already been replaced. Your custom config can be used to build for Remix"
+    );
+  } else {
+    const [originalEsbuildPath] =
+      require.resolve("esbuild").match(/^.*\/esbuild/) ?? [];
+    const [linkedEsbuildPath] =
+      require
+        .resolve("remix-esbuild-override")
+        .match(/^.*\/remix-esbuild-override/) ?? [];
+
+    execSync(
+      `mv ${originalEsbuildPath} ${originalEsbuildPath.replace(
+        /esbuild$/,
+        "esbuild-org"
+      )}`
+    );
+
+    execSync(`ln -s ${linkedEsbuildPath} ${originalEsbuildPath}`);
+
+    console.log(
+      "💽 Replaced esbuild. Your custom config can be used to build for Remix"
+    );
+  }
+};
