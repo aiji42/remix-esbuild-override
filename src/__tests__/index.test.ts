c6417ac36fb852c4d711ac5d69035d7ef0466183
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-24 00:04:33 +0900

    test: update test and install c8 for coverage

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,41 +2,108 @@ import { build } from "../index";
 import fs from "fs-extra";
 import path from "path";
 import * as Esbuild from "esbuild-org";
-
-const tempPath = path.join(__dirname, "../..", "tmp");
-const oldEnv = { ...process.env };
+import { beforeEach, describe } from "vitest";
 
 vi.mock("esbuild-org", () => ({
   build: vi.fn(),
 }));
 
+const tempPath = path.join(__dirname, "../..", "tmp");
+const oldEnv = { ...process.env };
+
+const prepareConfig = (script: string | undefined) => {
+  fs.removeSync(tempPath);
+  if (!script) return;
+  fs.mkdirSync(tempPath);
+  fs.writeFileSync(path.join(tempPath, "remix.config.js"), script);
+};
+
 describe("index", () => {
   beforeEach(() => {
     vi.resetAllMocks();
     process.env = { ...oldEnv, REMIX_ROOT: tempPath.toString() };
-    fs.mkdirSync(tempPath);
-    fs.writeFileSync(
-      path.join(tempPath, "remix.config.js"),
-      `
-      module.exports = {
-        esbuildOverride: (option, { isServer, isDev }) => {
-          option.runtime = isServer ? "server" : "browser"
-          option.mode = isDev ? "development" : "production"
-          return option
-        }
-      }
-    `
-    );
   });
   afterEach(() => {
     process.env = oldEnv;
     fs.removeSync(tempPath);
   });
 
-  test("build", async () => {
-    const mock = vi.spyOn(Esbuild, "build");
-    await build({});
+  describe("build", () => {
+    // TODO: need vi.resetModules()
+    test.skip("not override", async () => {
+      prepareConfig(`
+          module.exports = {}
+        `);
+      const mock = vi.spyOn(Esbuild, "build");
+      await build({});
 
-    expect(mock).toBeCalledWith({ mode: "production", runtime: "browser" });
+      expect(mock).toBeCalledWith({});
+    });
+
+    // TODO: need vi.resetModules()
+    test.skip("load config error", async () => {
+      prepareConfig(undefined);
+      expect(build({})).toThrowError();
+    });
+
+    describe("override", () => {
+      beforeEach(() => {
+        prepareConfig(`
+          module.exports = {
+            esbuildOverride: (option, { isServer, isDev }) => {
+              option.runtime = isServer ? "server" : "browser"
+              option.mode = isDev ? "development" : "production"
+              return option
+            }
+          }
+        `);
+      });
+
+      test("production/browser", async () => {
+        const mock = vi.spyOn(Esbuild, "build");
+        await build({});
+
+        expect(mock).toBeCalledWith({ mode: "production", runtime: "browser" });
+      });
+
+      test("production/server", async () => {
+        const mock = vi.spyOn(Esbuild, "build");
+        await build({ write: false });
+
+        expect(mock).toBeCalledWith({
+          mode: "production",
+          runtime: "server",
+          write: false,
+        });
+      });
+
+      test("development/browser", async () => {
+        const mock = vi.spyOn(Esbuild, "build");
+        await build({
+          define: { "process.env.NODE_ENV": "development" },
+        });
+
+        expect(mock).toBeCalledWith({
+          mode: "development",
+          runtime: "browser",
+          define: { "process.env.NODE_ENV": "development" },
+        });
+      });
+
+      test("development/server", async () => {
+        const mock = vi.spyOn(Esbuild, "build");
+        await build({
+          write: false,
+          define: { "process.env.NODE_ENV": "development" },
+        });
+
+        expect(mock).toBeCalledWith({
+          mode: "development",
+          runtime: "server",
+          write: false,
+          define: { "process.env.NODE_ENV": "development" },
+        });
+      });
+    });
   });
 });
