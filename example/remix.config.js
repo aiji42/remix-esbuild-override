691cde0bc146a4dfa621e5b0cc783152fbbea9fb
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-27 22:32:49 +0900

    chore: update example

--- example/remix.config.js
+++ example/remix.config.js
@@ -1,32 +1,32 @@
 const path = require("node:path");
 const alias = require("esbuild-plugin-alias");
-const { replaceEsbuild } = require("remix-esbuild-override/dist/replace");
+const { withEsbuildOverride } = require("remix-esbuild-override");
 
-replaceEsbuild();
+withEsbuildOverride((option, { isServer }) => {
+  console.log(option);
+  option.jsxFactory = "jsx";
+  option.inject = [path.resolve(__dirname, "reactShims.ts")];
+  option.plugins = [
+    alias({
+      through: require.resolve("no-op"),
+      "html-tokenize": require.resolve("no-op"),
+      multipipe: require.resolve("no-op"),
+    }),
+    ...option.plugins,
+  ];
+  if (isServer) option.mainFields = ["browser", "module", "main"];
+
+  return option;
+});
 
 /**
- * @type {import('remix-esbuild-override').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   serverBuildTarget: "cloudflare-workers",
   server: "./server.js",
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: [".*"],
-  esbuildOverride: (option, { isServer }) => {
-    option.jsxFactory = "jsx";
-    option.inject = [path.resolve(__dirname, "reactShims.ts")];
-    option.plugins = [
-      alias({
-        through: require.resolve("no-op"),
-        "html-tokenize": require.resolve("no-op"),
-        multipipe: require.resolve("no-op"),
-      }),
-      ...option.plugins,
-    ];
-    if (isServer) option.mainFields = ["browser", "module", "main"];
-
-    return option;
-  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
