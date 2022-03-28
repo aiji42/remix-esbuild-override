90d07d4c7e105144b72d77ff164501774bbd38b4
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-29 01:43:32 +0900

    test: update tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,70 +1,104 @@
-// import { build, withEsbuildOverride } from "../index";
-// import * as Esbuild from "esbuild-org";
-//
-// vi.mock("esbuild-org", () => ({
-//   build: vi.fn(),
-// }));
-// vi.mock("../replace", () => ({
-//   replaceEsbuild: vi.fn(),
-// }));
-//
-// describe("index", () => {
-//   afterEach(() => {
-//     vi.resetAllMocks();
-//   });
-//
-//   describe("build", () => {
-//     const mock = vi.spyOn(Esbuild, "build");
-//     beforeAll(() => {
-//       withEsbuildOverride((option, { isServer, isDev }) => {
-//         // @ts-ignore
-//         option.runtime = isServer ? "server" : "browser";
-//         // @ts-ignore
-//         option.mode = isDev ? "development" : "production";
-//         return option;
-//       });
-//     });
-//
-//     test("production/browser", async () => {
-//       await build({});
-//
-//       expect(mock).toBeCalledWith({ mode: "production", runtime: "browser" });
-//     });
-//
-//     test("production/server", async () => {
-//       await build({ write: false });
-//
-//       expect(mock).toBeCalledWith({
-//         mode: "production",
-//         runtime: "server",
-//         write: false,
-//       });
-//     });
-//
-//     test("development/browser", async () => {
-//       await build({
-//         define: { "process.env.NODE_ENV": "development" },
-//       });
-//
-//       expect(mock).toBeCalledWith({
-//         mode: "development",
-//         runtime: "browser",
-//         define: { "process.env.NODE_ENV": "development" },
-//       });
-//     });
-//
-//     test("development/server", async () => {
-//       await build({
-//         write: false,
-//         define: { "process.env.NODE_ENV": "development" },
-//       });
-//
-//       expect(mock).toBeCalledWith({
-//         mode: "development",
-//         runtime: "server",
-//         write: false,
-//         define: { "process.env.NODE_ENV": "development" },
-//       });
-//     });
-//   });
-// });
+import { withEsbuildOverride, esbuildOverrideOption } from "../index";
+import * as utils from "../utils";
+
+vi.mock("../utils");
+
+describe("index", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  describe("esbuildOverrideOption", () => {
+    beforeAll(() => {
+      vi.spyOn(utils, "load").mockReturnValue(null);
+      withEsbuildOverride((option, { isServer, isDev }) => {
+        // @ts-ignore
+        option.runtime = isServer ? "server" : "browser";
+        // @ts-ignore
+        option.mode = isDev ? "development" : "production";
+        return option;
+      });
+    });
+
+    test("production/browser", () => {
+      expect(esbuildOverrideOption({})).toEqual({
+        mode: "production",
+        runtime: "browser",
+      });
+    });
+
+    test("production/server", () => {
+      expect(esbuildOverrideOption({ write: false })).toEqual({
+        mode: "production",
+        runtime: "server",
+        write: false,
+      });
+    });
+
+    test("development/browser", () => {
+      expect(
+        esbuildOverrideOption({
+          define: { "process.env.NODE_ENV": "development" },
+        })
+      ).toEqual({
+        mode: "development",
+        runtime: "browser",
+        define: { "process.env.NODE_ENV": "development" },
+      });
+    });
+
+    test("development/server", () => {
+      expect(
+        esbuildOverrideOption({
+          write: false,
+          define: { "process.env.NODE_ENV": "development" },
+        })
+      ).toEqual({
+        mode: "development",
+        runtime: "server",
+        write: false,
+        define: { "process.env.NODE_ENV": "development" },
+      });
+    });
+  });
+
+  describe("withEsbuildOverride", () => {
+    let esbuild: any;
+    let mockedBuildFunction = vi.fn();
+    beforeAll(() => {
+      vi.spyOn(utils, "load").mockImplementation((mod) => {
+        if (mod === "@remix-run/dev/node_modules/esbuild") return esbuild;
+        return null;
+      });
+    });
+
+    test("override esbuild.build", () => {
+      esbuild = { build: mockedBuildFunction };
+      withEsbuildOverride((option) => {
+        option.jsxFactory = "jsx";
+        return option;
+      });
+      esbuild.build({ foo: "bar" });
+
+      expect(mockedBuildFunction).toBeCalledWith({
+        foo: "bar",
+        jsxFactory: "jsx",
+      });
+      expect(esbuild.overridden).toEqual(true);
+    });
+
+    test("esbuild has already been overwritten", () => {
+      esbuild = { build: mockedBuildFunction, overridden: true };
+      withEsbuildOverride((option) => {
+        option.jsxFactory = "jsx";
+        return option;
+      });
+      esbuild.build({ foo: "bar" });
+
+      expect(mockedBuildFunction).toBeCalledWith({
+        foo: "bar",
+      });
+      expect(esbuild.overridden).toEqual(true);
+    });
+  });
+});
---
+++ src/__tests__/patching.test.ts
@@ -0,0 +1,91 @@
+import { fs, vol } from "memfs";
+import { patching } from "../patching";
+import * as utils from "../utils";
+
+const esbuildMainJs = `
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+`;
+
+vi.mock("../utils");
+vi.mock("fs", () => fs);
+// const mockedConsoleLog = vi.spyOn(console, "log");
+
+beforeEach(() => {
+  vol.reset();
+});
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+test("Only esbuild directly under node_modules", () => {
+  vol.fromJSON(
+    {
+      "./node_modules/esbuild/package.json":
+        '{ "name": "esbuild", "main": "lib/main.js" }',
+      "./node_modules/esbuild/lib/main.js": esbuildMainJs,
+    },
+    "/app"
+  );
+  vi.spyOn(utils, "resolve").mockImplementation((mod) => {
+    if (mod === "esbuild") return "/app/node_modules/esbuild/lib/main.js";
+    return null;
+  });
+
+  patching();
+
+  expect(vol.toJSON()).toMatchSnapshot();
+});
+
+test("Only esbuild under @remix-run/dev", () => {
+  vol.fromJSON(
+    {
+      "./node_modules/@remix-run/dev/node_modules/esbuild/package.json":
+        '{ "name": "esbuild", "main": "lib/main.js" }',
+      "./node_modules/@remix-run/dev/node_modules/esbuild/lib/main.js":
+        esbuildMainJs,
+    },
+    "/app"
+  );
+  vi.spyOn(utils, "resolve").mockImplementation((mod) => {
+    if (mod === "@remix-run/dev/node_modules/esbuild")
+      return "/app/node_modules/@remix-run/dev/node_modules/esbuild/lib/main.js";
+    return null;
+  });
+
+  patching();
+
+  expect(vol.toJSON()).toMatchSnapshot();
+});
+
+test("esbuild exists in both", () => {
+  vol.fromJSON(
+    {
+      "./node_modules/esbuild/package.json":
+        '{ "name": "esbuild", "main": "lib/main.js" }',
+      "./node_modules/esbuild/lib/main.js": esbuildMainJs,
+      "./node_modules/@remix-run/dev/node_modules/esbuild/package.json":
+        '{ "name": "esbuild", "main": "lib/main.js" }',
+      "./node_modules/@remix-run/dev/node_modules/esbuild/lib/main.js":
+        esbuildMainJs,
+    },
+    "/app"
+  );
+  vi.spyOn(utils, "resolve").mockImplementation((mod) => {
+    if (mod === "@remix-run/dev/node_modules/esbuild")
+      return "/app/node_modules/@remix-run/dev/node_modules/esbuild/lib/main.js";
+    return null;
+  });
+
+  patching();
+
+  expect(vol.toJSON()).toMatchSnapshot();
+});
