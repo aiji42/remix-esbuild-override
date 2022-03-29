6df9046756f717d922c9e6975293703bdf69bfde
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-29 22:16:40 +0900

    chore: add jsDoc comments #11

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,35 @@ import { mods } from "./constants";
 type BrowserBuildOption = BuildOptions;
 type ServerBuildOption = BuildOptions & { write: false };
 type EsbuildOption = BrowserBuildOption | ServerBuildOption;
+/**
+ * @property {boolean} isServer - Whether the esbuild call is for server-side
+ * @property {boolean} isDev - Whether the esbuild call is for a development
+ */
 type EsbuildContext = {
   isServer: boolean;
   isDev: boolean;
 };
+
+/**
+ * @callback EsbuildOverride
+ * @param {EsbuildOption} option - Default option values to be used when remix calls esbuild
+ * @param {EsbuildContext} context - @see {@link EsbuildContext}
+ * @return {EsbuildOption} - Return the option values you override
+ */
 export type EsbuildOverride = (
   option: EsbuildOption,
   context: EsbuildContext
 ) => EsbuildOption;
 
-let esbuildOverride: EsbuildOverride = (arg) => arg;
+let _esbuildOverride: EsbuildOverride = (arg) => arg;
 
-export const withEsbuildOverride = (_esbuildOverride?: EsbuildOverride) => {
-  if (typeof _esbuildOverride !== "function") return;
-  esbuildOverride = _esbuildOverride;
+/**
+ * This is a function to override esbuild; add it to remix.config.js.
+ * @param {EsbuildOverride} esbuildOverride - callback function
+ */
+export const withEsbuildOverride = (esbuildOverride?: EsbuildOverride) => {
+  if (typeof esbuildOverride !== "function") return;
+  _esbuildOverride = esbuildOverride;
 
   for (const mod of mods) {
     const esbuild = load<{ overridden?: boolean; build: typeof build }>(mod);
@@ -46,5 +61,5 @@ export const withEsbuildOverride = (_esbuildOverride?: EsbuildOverride) => {
 export const esbuildOverrideOption = (option: EsbuildOption) => {
   const isServer = option.write === false;
   const isDev = option.define?.["process.env.NODE_ENV"] === "development";
-  return esbuildOverride(option, { isServer, isDev });
+  return _esbuildOverride(option, { isServer, isDev });
 };
