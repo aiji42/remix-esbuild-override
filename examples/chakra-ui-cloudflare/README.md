09e53f4cf14f86be1f556a5cf5903b754aaca867
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-30 23:46:36 +0900

    chore: add example for chakra-ui on cloudflare

---
+++ examples/chakra-ui-cloudflare/README.md
@@ -0,0 +1,107 @@
+# Remix with [Chakra-ui](https://chakra-ui.com/) on Cloudflare
+
+The base template is based on the official [setup document](https://chakra-ui.com/guides/getting-started/remix-guide) and is exactly as it is under the app directory.  
+This example is for Cloudflare Workers, but it is basically the same for Cloudflare Pages.
+
+Since @emotion/server depends on Buffer, it cannot run on Cloudflare as is.
+
+## Use remix-esbuild-override
+
+Use remix-esbuild-override and the plugin for polyfill to solve the problem.
+
+1. Install and setup `postinstall`
+
+```bash
+npm install -D remix-esbuild-override @esbuild-plugins/node-globals-polyfill
+# or
+yarn add -D remix-esbuild-override @esbuild-plugins/node-globals-polyfill
+```
+
+Update `scripts > postinstall` in package.json.
+
+```json
+"scripts": {
+  "postinstall": "remix setup cloudflare && remix-esbuild-override"
+}
+```
+
+Run `npm install` or `yarn install` again to run `postinstall`.
+
+2. Update remix.config.js
+
+```js
+const { withEsbuildOverride } = require("remix-esbuild-override");
+const GlobalsPolyfills =
+  require("@esbuild-plugins/node-globals-polyfill").default;
+
+withEsbuildOverride((option, { isServer }) => {
+  if (isServer) option.mainFields = ["browser", "module", "main"];
+
+  option.plugins = [
+    GlobalsPolyfills({
+      buffer: true,
+    }),
+    ...option.plugins,
+  ];
+
+  return option;
+});
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  serverBuildTarget: "cloudflare-workers",
+  server: "./server.js",
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: [".*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+};
+```
+
+That's all.
+
+---
+
+# Welcome to Remix!
+
+- [Remix Docs](https://remix.run/docs)
+
+## Development
+
+You will be running two processes during development:
+
+- The Miniflare server (miniflare is a local environment for Cloudflare Workers)
+- The Remix development server
+
+Both are started with one command:
+
+```sh
+npm run dev
+```
+
+Open up [http://127.0.0.1:8787](http://127.0.0.1:8787) and you should be ready to go!
+
+If you want to check the production build, you can stop the dev server and run following commands:
+
+```sh
+npm run build
+npm start
+```
+
+Then refresh the same URL in your browser (no live reload for production builds).
+
+## Deployment
+
+Use [wrangler](https://developers.cloudflare.com/workers/cli-wrangler) to build and deploy your application to Cloudflare Workers. If you don't have it yet, follow [the installation guide](https://developers.cloudflare.com/workers/cli-wrangler/install-update) to get it setup. Be sure to [authenticate the CLI](https://developers.cloudflare.com/workers/cli-wrangler/authentication) as well.
+
+If you don't already have an account, then [create a cloudflare account here](https://dash.cloudflare.com/sign-up) and after verifying your email address with Cloudflare, go to your dashboard and set up your free custom Cloudflare Workers subdomain.
+
+Once that's done, you should be able to deploy your app:
+
+```sh
+npm run deploy
+```
---
+++ examples/chakra-ui-cloudflare/app/root.tsx
@@ -0,0 +1,124 @@
+import React, { useContext, useEffect } from "react";
+import {
+  Outlet,
+  Meta,
+  Links,
+  Scripts,
+  ScrollRestoration,
+  LiveReload,
+  MetaFunction,
+  LinksFunction,
+  useCatch,
+} from "remix";
+import { withEmotionCache } from "@emotion/react";
+import { Box, ChakraProvider, Heading } from "@chakra-ui/react";
+import { ServerStyleContext, ClientStyleContext } from "./styles/context";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Remix with Chakra-ui",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstaticom",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+    },
+  ];
+};
+
+interface DocumentProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+const Document = withEmotionCache(
+  ({ children, title }: DocumentProps, emotionCache) => {
+    const serverStyleData = useContext(ServerStyleContext);
+    const clientStyleData = useContext(ClientStyleContext);
+
+    // Only executed on client
+    useEffect(() => {
+      // re-link sheet container
+      emotionCache.sheet.container = document.head;
+      // re-inject tags
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+      // reset cache to reapply global styles
+      clientStyleData?.reset();
+    }, []);
+
+    return (
+      <html lang="en">
+        <head>
+          {title ? <title>{title}</title> : null}
+          <Meta />
+          <Links />
+          {serverStyleData?.map(({ key, ids, css }) => (
+            <style
+              key={key}
+              data-emotion={`${key} ${ids.join(" ")}`}
+              dangerouslySetInnerHTML={{ __html: css }}
+            />
+          ))}
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
+
+export default function App() {
+  return (
+    <Document>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="purple.600">
+            [CatchBoundary]: {caught.status} {caught.statusText}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Error!">
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="blue.500">
+            [ErrorBoundary]: There was an error: {error.message}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
