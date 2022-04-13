08d928f5205b660473d5d194c194ecb25f51dcef
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-04-13 22:57:47 +0900

    test: update reporter

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: ["text", "json", "html", "lcov"],
+    },
   },
 });
