65acf7d2373d49c424ecc975b4859e3c595bb2a0
Morten Barklund <morten.barklund@gmail.com>
2022-10-14 17:11:51 +0200

    feat: Added example for how to use Styled-Components
    This example is build on the vanilla-extract example, but using a few extra bits here and there.

---
+++ examples/styled-components/app/routes/index.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  color: hotpink;
+`;
+
+export default function Index() {
+  return (
+    <div>
+      <Heading>Welcome to Remix using Styled-Components</Heading>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
